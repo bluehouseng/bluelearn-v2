6dbfa1a6112cd1b03e2c52b878028e173704480b
jothamardel <ardelmbiplang@gmail.com>
2024-09-20 12:35:55 +0100

    chore: updating application
    Signed-off-by: jothamardel <ardelmbiplang@gmail.com>

--- components/faqs.tsx
+++ components/faqs.tsx
@@ -5,53 +5,86 @@ export default function Faqs() {
         <div className="pb-12 md:pb-20">
           {/* Section header */}
           <div className="max-w-3xl mx-auto pb-10">
-            <h2 className="h2 font-cabinet-grotesk">Frequently asked questions</h2>
+            <h2 className="h2 font-cabinet-grotesk">
+              Frequently asked questions
+            </h2>
           </div>
 
           <div className="max-w-3xl mx-auto space-y-6">
             {/* Item */}
             <div className="space-y-2">
-              <h4 className="text-xl font-cabinet-grotesk font-bold">How much is this going to cost?</h4>
+              <h4 className="text-xl font-cabinet-grotesk font-bold">
+                How long does it usually take for me to build something and get
+                hired?
+              </h4>
               <p className="text-gray-500">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua, ut enim ad
-                minim veniam.
+                Ultimately you’re the only one that can control that. Ok fine...
+                <br />
+                ... everyone learns at a different pace, but students who put in
+                2-3 hours each day to apply what they’re learning in the
+                lectures should be able to confidently build their own projects,
+                start interviewing and get hired in 3-6 months
+                <br />
+                We don’t see just getting hired as the end goal though. We
+                believe in lifelong learning. Our intermediate and advanced
+                courses will teach you the topics and skills you need to get
+                promoted and take your career to the next level.
               </p>
             </div>
             {/* Item */}
             <div className="space-y-2">
-              <h4 className="text-xl font-cabinet-grotesk font-bold">What is the difference between the basic and pro plans?</h4>
+              <h4 className="text-xl font-cabinet-grotesk font-bold">
+                Can I use the course projects in my portfolio?
+              </h4>
               <p className="text-gray-500">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua, ut enim ad
-                minim veniam.
+                Yes, you’d be crazy not to in our slightly biased opinion! All
+                projects are downloadable and ready to use the minute you join.
+                <br />
+                Many of our students tell us the projects they built while
+                following along with our courses were what got them interviews
+                and because they built the projects themselves, they could
+                confidently explain and walk through their work during the
+                interview.
+                <br />
+                <strong>You know what that means? Job offer!</strong>
               </p>
             </div>
             {/* Item */}
             <div className="space-y-2">
-              <h4 className="text-xl font-cabinet-grotesk font-bold">Do you have student or non-profit discounts?</h4>
+              <h4 className="text-xl font-cabinet-grotesk font-bold">
+                Do you have student or non-profit discounts?
+              </h4>
               <p className="text-gray-500">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua, ut enim ad
-                minim veniam.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua, ut
+                enim ad minim veniam.
               </p>
             </div>
             {/* Item */}
             <div className="space-y-2">
-              <h4 className="text-xl font-cabinet-grotesk font-bold">How is the price determined?</h4>
+              <h4 className="text-xl font-cabinet-grotesk font-bold">
+                How is the price determined?
+              </h4>
               <p className="text-gray-500">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua, ut enim ad
-                minim veniam.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua, ut
+                enim ad minim veniam.
               </p>
             </div>
             {/* Item */}
             <div className="space-y-2">
-              <h4 className="text-xl font-cabinet-grotesk font-bold">Have more questions?</h4>
+              <h4 className="text-xl font-cabinet-grotesk font-bold">
+                Have more questions?
+              </h4>
               <p className="text-gray-500">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua, ut enim ad
-                minim veniam.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua, ut
+                enim ad minim veniam.
               </p>
             </div>
           </div>
         </div>
       </div>
     </section>
-  )
+  );
 }
